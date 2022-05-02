b314e085e750396cd869ee94b95fc2f1d4fa48c4
vokamis <asimakov@twelvechairs-software.com>
2022-05-02 18:22:58 -0400

    RPI

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+sed -i 's/..\/src\/include\/unicorn.gltf/home\/pi\/src\/include\/unicorn.gltf/g' src/main.cpp
+
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel -G "CodeBlocks - Unix Makefiles" .
 cmake --build . --target learn_raylib --clean-first -- -j 4
+
+./learn_raylib
