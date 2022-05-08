545537d6242af781329c60734d396dc9dbc7b8d6
vokamis <asimakov@twelvechairs-software.com>
2022-05-08 17:30:18 -0400

    Models: new

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
 #define RAYGUI_IMPLEMENTATION
@@ -54,18 +33,28 @@ int main()
 
     // Define the camera to look into our 3d world
     Camera camera = { 0 };
-    camera.position = (Vector3){ 1.0f, 5.0f, 1.0f }; // Camera position
+    camera.position = (Vector3){ 1.0f, 8.0f, 1.0f }; // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-    Model unicornBody = LoadModel("../src/include/unicorn.obj");                 // Load OBJ model
+    Model unicornBody = LoadModel("../src/include/unicorn_body.obj");
+    Model unicornHorn = LoadModel("../src/include/unicorn_horn.obj");
+    Model unicornMane = LoadModel("../src/include/unicorn_mane.obj");
+    Model unicornTail = LoadModel("../src/include/unicorn_tail.obj");
     Vector3 unicornBodyPosition = {0.0f, 0.0f, 0.0f };
+    Vector3 unicornHornPosition = {0.0f, 0.0f, 0.0f };
+    Vector3 unicornManePosition = {0.0f, 0.0f, 0.0f };
+    Vector3 unicornTailPosition = {0.0f, 0.0f, 0.0f };
 
     SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
 
-    auto mainColor = PINK;
+    auto colorPaneActive = true;
+    auto unicornBodyColor = BLUE;
+    auto unicornHornColor = GREEN;
+    auto unicornManeColor = PINK;
+    auto unicornTailColor = YELLOW;
     RenderTexture2D target = LoadRenderTexture(320, 240);
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -74,9 +63,11 @@ int main()
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        if (IsKeyPressed(KEY_ONE)) colorPaneActive = !colorPaneActive;
+
         BeginDrawing();
 
-        ClearBackground(DARKGRAY);
+        ClearBackground(BLACK);
 
         // Render generated texture using selected postprocessing shader
 //        BeginShaderMode(1);
@@ -84,19 +75,27 @@ int main()
 //        DrawTextureRec(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, (float)-target.texture.height }, (Vector2){ 0, 0 }, WHITE);
 //        EndShaderMode();
 
+
+        GuiWindowBox((Rectangle) {121, 0, 200, 240}, "PUDGY");
+
         BeginMode3D(camera);
+//        rlViewport(121, 0, 200, 240);
         const float radius = 10.0f;
         float camX = (float)sin(GetTime()) * radius;
         float camZ = (float)cos(GetTime()) * radius;
-        camera.position = (Vector3){camX, 5.0f, camZ};
-//        rlScalef(1.2f, 1.2f, 1.2f);
-        DrawModel(unicornBody, unicornBodyPosition, 1.0f, mainColor);
+        camera.position = (Vector3){camX, 8.0f, camZ};
+        DrawModel(unicornBody, unicornBodyPosition, 1.0f, unicornBodyColor);
+        DrawModel(unicornHorn, unicornBodyPosition, 1.0f, unicornHornColor);
+        DrawModel(unicornMane, unicornBodyPosition, 1.0f, unicornManeColor);
+        DrawModel(unicornTail, unicornBodyPosition, 1.0f, unicornTailColor);
         EndMode3D();
-        GuiDrawRectangle((Rectangle){0, 0, 120, 120}, 1, BLACK, LIGHTGRAY);
-        DrawText(" Sofia's Pudgy Unicorn!", 2, 2, 11, GRAY);
-        DrawText(" Sofia's Pudgy Unicorn!", 1, 1, 11, BLUE);
-        mainColor = GuiColorPicker((Rectangle){0, 120, 120, 120}, nullptr, mainColor);
 
+        if (colorPaneActive) {
+            GuiDrawRectangle((Rectangle) {0, 0, 120, 120}, 1, BLACK, WHITE);
+            DrawText(" Sofia's Pudgy Unicorn!", 2, 2, 10, GRAY);
+            DrawText(" Sofia's Pudgy Unicorn!", 1, 1, 10, BLUE);
+            unicornBodyColor = GuiColorPicker((Rectangle) {0, 120, 120, 120}, nullptr, unicornBodyColor);
+        }
         EndDrawing();
     }
 
