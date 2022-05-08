a9e4945858e686e0ef67b4d487be552d9b3c18f8
vokamis <asimakov@twelvechairs-software.com>
2022-05-08 17:34:29 -0400

    Models: new

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-sed -i 's/..\/src\/include\/unicorn.obj/src\/include\/unicorn.obj/g' src/main.cpp
+sed -i 's/..\/src\/include\/unicorn_body.obj/src\/include\/unicorn_body.obj/g' src/main.cpp
+sed -i 's/..\/src\/include\/unicorn_horn.obj/src\/include\/unicorn_horn.obj/g' src/main.cpp
+sed -i 's/..\/src\/include\/unicorn_mane.obj/src\/include\/unicorn_mane.obj/g' src/main.cpp
+sed -i 's/..\/src\/include\/unicorn_tail.obj/src\/include\/unicorn_tail.obj/g' src/main.cpp
 
 export CONAN_SYSREQUIRES_MODE=enabled
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel -G "CodeBlocks - Unix Makefiles" .
