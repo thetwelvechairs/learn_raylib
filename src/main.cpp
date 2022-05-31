1478333d78cbd29b01fd77f377b5f2fd19fa5c84
vokamis <asimakov@twelvechairs-software.com>
2022-05-30 21:18:59 -0400

    JAC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "raylib.h"
+#include <cstdlib>
 
 #define RAYGUI_IMPLEMENTATION
+
+#if defined(PLATFORM_DESKTOP)
+    #define GLSL_VERSION            330
+#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
+    #define GLSL_VERSION            100
+#endif
+
 #include "include/raygui.h"
 #include "rlgl.h"
 #include <random>
@@ -24,24 +32,42 @@
     return vect[ (i+1) % v.size() ];
 }
 
+auto rayHit(Camera camera, Ray ray, Model model){
+    ray = GetMouseRay(GetMousePosition(), camera);
+    // Check collision between ray and model
+    return GetRayCollisionModel(ray, model);
+}
+
 int main(){
     const int screenWidth = 320;
     const int screenHeight = 240;
 
     std::string title = std::to_string(screenWidth) + "x" + std::to_string(screenHeight);
     InitWindow(screenWidth, screenHeight, title.c_str());
-
+    std::vector<Model> models;
     Model unicornBody = LoadModel("../src/include/unicorn_body.obj");
+    models.push_back(unicornBody);
     Model unicornHorn = LoadModel("../src/include/unicorn_horn.obj");
+    models.push_back(unicornHorn);
     Model unicornMane = LoadModel("../src/include/unicorn_mane.obj");
+    models.push_back(unicornMane);
     Model unicornTail = LoadModel("../src/include/unicorn_tail.obj");
-    Vector3 unicornBodyPosition = {0.0f, 0.0f, 0.0f };
+    models.push_back(unicornTail);
+
+    enum unicornParts {
+        body = 0,
+        mane,
+        tail,
+        horn
+    } unicornParts;
+
+    Vector3 unicornBodyPosition = {0.0f, 0.0f, 0.0f};
 
     // Define the camera to look into our 3d world
     Camera camera = {0};
     auto cameraHeight = 2.0f;
-    camera.position = (Vector3){0.0f, cameraHeight, 0.0f };
-    camera.target = (Vector3){0.0f, cameraHeight, 0.0f };
+    camera.position = (Vector3){0.0f, cameraHeight, 0.0f};
+    camera.target = (Vector3){0.0f, cameraHeight, 0.0f};
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
     camera.fovy = 35.0f;
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
@@ -53,43 +79,51 @@ int main(){
     auto unicornHornColor = GREEN;
     auto unicornManeColor = YELLOW;
     auto unicornTailColor = BLUE;
-    std::vector<std::string> unicornParts = {"Body", "Mane", "Tail", "Horn"};
-    std::string selectedPart = "Body";
+
+    int selectedPart = -1;
 
     auto clicked = false;
 
-    Vector3 cubePosition = { 0.0f, 1.0f, 0.0f };
-    Vector3 cubeSize = { 2.0f, 2.0f, 2.0f };
-    Ray ray = { 0 };                    // Picking line ray
-    RayCollision collision = { false };
+    Vector3 cubePosition = {0.0f, 1.0f, 0.0f};
+    Vector3 cubeSize = {2.0f, 2.0f, 2.0f};
+    Ray ray = {0};
+    RayCollision collision = {false};
+
+    int collisionModelIndex = 0;
+
+    auto texture = LoadRenderTexture(screenWidth, screenHeight);
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()){
         if (IsKeyPressed(KEY_ONE)) colorPaneActive = !colorPaneActive;
 //        if (IsKeyPressed(KEY_TWO)) selectedPart = unicornParts.;
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            if (!collision.hit)
-            {
-                ray = GetMouseRay(GetMousePosition(), camera);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+//            if (!collision.hit){
 
-                // Check collision between ray and box
-                collision = GetRayCollisionBox(ray,
-                                               (BoundingBox){(Vector3){ cubePosition.x - cubeSize.x/2, cubePosition.y - cubeSize.y/2, cubePosition.z - cubeSize.z/2 },
-                                                             (Vector3){ cubePosition.x + cubeSize.x/2, cubePosition.y + cubeSize.y/2, cubePosition.z + cubeSize.z/2 }});
-            }
-            else collision.hit = false;
+                for (int n = 0; n < models.size(); n++) {
+                    collision = rayHit(camera, ray, models.at(n));
+                    if (collision.hit) {
+                        colorPaneActive = true;
+                        collisionModelIndex = n;
+                    }
+                    else {
+                        colorPaneActive = false;
+                        collisionModelIndex = -1;
+                    }
+                }
+//            }
+//            else collision.hit = false;
         }
 
         BeginDrawing();
 
         ClearBackground(WHITE);
 
+//        BeginTextureMode(texture);
         BeginMode3D(camera);
         const float radius = 10.0f;
         const float speedFactor = 4.0f;
@@ -103,50 +137,59 @@ int main(){
 
         DrawGrid(14.0f, 0.5f);
 
-        if (collision.hit){
-            DrawCubeWires(unicornBodyPosition, cubeSize.x, cubeSize.y, cubeSize.z, MAROON);
-            DrawCubeWires(unicornBodyPosition, cubeSize.x + 0.2f, cubeSize.y + 0.2f, cubeSize.z + 0.2f, GREEN);
-            DrawCubeWires(unicornBodyPosition, cubeSize.x + 0.3f, cubeSize.y + 0.3f, cubeSize.z + 0.3f, GREEN);
-            DrawCubeWires(unicornBodyPosition, cubeSize.x + 0.4f, cubeSize.y + 0.4f, cubeSize.z + 0.4f, GREEN);
-        }
+//        if (collision.hit){
+//            DrawCubeWires(unicornBodyPosition, cubeSize.x, cubeSize.y, cubeSize.z, MAROON);
+//            DrawCubeWires(unicornBodyPosition, cubeSize.x + 0.2f, cubeSize.y + 0.2f, cubeSize.z + 0.2f, GREEN);
+//            DrawCubeWires(unicornBodyPosition, cubeSize.x + 0.3f, cubeSize.y + 0.3f, cubeSize.z + 0.3f, GREEN);
+//            DrawCubeWires(unicornBodyPosition, cubeSize.x + 0.4f, cubeSize.y + 0.4f, cubeSize.z + 0.4f, GREEN);
+//        }
 
         EndMode3D();
+//        EndTextureMode();
+//        DrawTexture(texture.texture, screenWidth, screenHeight, BLACK);
 
         if (colorPaneActive){
-            if (selectedPart == "Body"){
+            if (collisionModelIndex == 0){
                 unicornBodyColor = GuiColorPicker((Rectangle) {170, 110, 120, 120}, nullptr, unicornBodyColor);
+                GuiButton((Rectangle) {0, 1, 50, 20}, "Body");
             }
-            if (selectedPart == "Horn"){
+            else if (collisionModelIndex == 1){
                 unicornHornColor = GuiColorPicker((Rectangle) {170, 110, 120, 120}, nullptr, unicornHornColor);
+                GuiButton((Rectangle) {0, 1, 50, 20}, "Hair");
             }
-            if (selectedPart == "Mane"){
+            else if (collisionModelIndex == 2){
                 unicornManeColor = GuiColorPicker((Rectangle) {170, 110, 120, 120}, nullptr, unicornManeColor);
+                GuiButton((Rectangle) {0, 1, 50, 20}, "Mane");
             }
-            if (selectedPart == "Tail"){
+            else if (collisionModelIndex == 3){
                 unicornTailColor = GuiColorPicker((Rectangle) {170, 110, 120, 120}, nullptr, unicornTailColor);
+                GuiButton((Rectangle) {0, 1, 50, 20}, "Tail");
+            }
+            else{
+                break;
             }
         }
 
 
-        GuiSetStyle(BUTTON, BORDER_WIDTH, 2);
-        GuiSetStyle(BUTTON, TEXT_ALIGNMENT, GUI_TEXT_ALIGN_CENTER);
-
-        if (GuiButton((Rectangle) {0, 1, 50, 20}, "Body")){
-            selectedPart = "Body";
-            colorPaneActive = !colorPaneActive;
-        }
-        if (GuiButton((Rectangle) {0, 62, 50, 20}, "Horn")){
-            selectedPart = "Horn";
-            colorPaneActive = !colorPaneActive;
-        }
-        if (GuiButton((Rectangle) {0, 122, 50, 20}, "Tail")){
-            selectedPart = "Tail";
-            colorPaneActive = !colorPaneActive;
-        }
-        if (GuiButton((Rectangle) {0, 186, 50, 20}, "Mane")){
-            selectedPart = "Mane";
-            colorPaneActive = !colorPaneActive;
-        }
+//        GuiSetStyle(BUTTON, BORDER_WIDTH, 2);
+//        GuiSetStyle(BUTTON, TEXT_ALIGNMENT, GUI_TEXT_ALIGN_CENTER);
+//
+//        if (GuiButton((Rectangle) {0, 1, 50, 20}, "Body")){
+//            selectedPart = 0;
+//            colorPaneActive = !colorPaneActive;
+//        }
+//        if (GuiButton((Rectangle) {0, 62, 50, 20}, "Horn")){
+//            selectedPart = 1;
+//            colorPaneActive = !colorPaneActive;
+//        }
+//        if (GuiButton((Rectangle) {0, 122, 50, 20}, "Tail")){
+//            selectedPart = 2;
+//            colorPaneActive = !colorPaneActive;
+//        }
+//        if (GuiButton((Rectangle) {0, 186, 50, 20}, "Mane")){
+//            selectedPart = 3;
+//            colorPaneActive = !colorPaneActive;
+//        }
 
         EndDrawing();
     }
